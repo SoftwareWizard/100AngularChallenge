01694f3f2b10e652b0c2427d43a85a99e37b3ea5
Simon Bauer <simon.bauer@doka.com>
2021-01-10 08:47:43 -0500

    fix banner-cut-out component

--- src/app/modules/components/banner-cut-out/banner-cut-out.component.ts
+++ src/app/modules/components/banner-cut-out/banner-cut-out.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./banner-cut-out.component.scss'],
 })
 export class BannerCutOutComponent {
-  @Input() public imageUrl = '';
+  @Input() public imageUrl =
+    'https://images.unsplash.com/photo-1610208311724-72e7baf69795?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
   @Input() public title = '100 Angular Challenge';
 }
