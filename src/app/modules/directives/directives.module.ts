3f22cc0aaf01da03cd8a67562f8fc4b7225122a5
Simon Bauer <simon.bauer@doka.com>
2021-01-10 11:36:52 -0500

    add lazy load image directive

--- src/app/modules/directives/directives.module.ts
+++ src/app/modules/directives/directives.module.ts
@@ -8,6 +8,7 @@ import { DebounceClickDirective } from './debounce-click/debounce-click.directiv
 import { RippleDirective } from './ripple/ripple.directive';
 import { ScaleDirective } from './scale/scale.directive';
 import { CopyDirective } from './copy/copy.directive';
+import { LazyLoadImageDirective } from './lazy-load-image/lazy-load-image.directive';
 
 @NgModule({
   declarations: [
@@ -16,11 +17,13 @@ import { CopyDirective } from './copy/copy.directive';
     RippleDirective,
     ScaleDirective,
     CopyDirective,
+    LazyLoadImageDirective,
   ],
   imports: [CommonModule, DirectivesRoutingModule, SharedModule],
   providers: [
     { provide: 'Navigator', useValue: navigator },
     { provide: 'Document', useValue: document },
+    { provide: 'Window', useValue: window },
   ],
 })
 export class DirectivesModule {}
